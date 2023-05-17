605be3b77c2ac7d33c6a998dce7b132d61b87113
Lyssa <aizzalizza@gmail.com>
2023-05-17 23:34:44 +0000

    added runtime

--- personal-projects/runtime/js/runLevels.js
+++ personal-projects/runtime/js/runLevels.js
@@ -18,18 +18,9 @@ var runLevels = function (window) {
 
     // TODOs 5 through 11 go here
     // BEGIN EDITING YOUR CODE HERE
-    var hitZoneSize = 25;
-    var damageFromObstacle = 10;
-    var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
-    sawBladeHitZone.x = 400;
-    sawBladeHitZone.y = 400;
-    game.addGameItem(sawBladeHitZone);
-    var obstacleImage = draw.bitmap("img/sawblade.png");
-    sawBladeHitZone.addChild(obstacleImage);
-    obstacleImage.x = -25;
-    obstacleImage.y = -25;
 
     
+
     function startLevel() {
       // TODO 13 goes below here
 
