3f58c81a2e2b273c8df28e56e13e69c1675e7372
Lyssa <aizzalizza@gmail.com>
2023-06-07 16:31:12 +0000

    saving debugging exercise

--- asd-projects/debugging-exercise/index.js
+++ asd-projects/debugging-exercise/index.js
@@ -108,7 +108,7 @@ function update(){
 
 // this moves circles in memory but doesn't update them on the screen
 function moveCircle(circle){
-    circle.x = circle.speedX;
+    circle.x += circle.speedX;
     circle.y += circle.speedY;
 }
 
@@ -133,13 +133,13 @@ function bounceCircle(circle){
     // this bounces off the bottom wall
     else if (circle.y > boardHeight){
         circle.y -= circle.speedY;
-        circle.speedX *= -1;
+        circle.speedY *= -1;
     }
 }
 
 // this redraws the circle's position on the screen
 function updateCircleOnScreen(circle){
-    maxCircles = 0;
+    // maxCircles = 0;
 
     // these lines redraw the circle's position
     $(circle.id).css('left', circle.x);
