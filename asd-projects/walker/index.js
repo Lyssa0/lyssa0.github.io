0eb22bc401a07231df02a28cf53fe8c74762ffe1
Lyssa <aizzalizza@gmail.com>
2023-06-16 00:45:39 +0000

    update

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -10,6 +10,17 @@ function runProgram(){
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
+  const KEY = {
+    LEFT: 37,
+    RIGHT: 39,
+    UP: 38,
+    DOWN: 40,
+  }
+  var speedX = 0;
+  var speedY = 0;
+  var positionX = 0;
+  var positionY = 0;
+
   // Game Item Objects
 
 
@@ -26,18 +37,43 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
+    repositionGameItem()
+    redrawGameItem()
 
   }
   
   /* 
   Called in response to events.
   */
-  
+  function handleKeyDown(event){
+    if (event.which === KEY.LEFT){
+      console.log("left pressed");
+      speedX = -5;
+    }else if(event.which === KEY.RIGHT){
+      console.log("right pressed");
+      speedX = 5;
+    }else if(event.which === KEY.DOWN){
+      console.log("down pressed");
+      speedY = 5;
+    }else if(event.which === KEY.UP){
+      console.log("up pressed");
+      speedY = -5;
+    }
+    
   }
+  // }
+  
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
+  function repositionGameItem(){
+    positionX += speedX;
+    positionY += speedY;
+  };
+  function redrawGameItem(){
+    $("#walker").css("left", positionX);
+    $("#walker").css("top", positionY);
+  };
   
   function endGame() {
     // stop the interval timer
